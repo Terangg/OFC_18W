124adc4aa6b6826c69aded67dcde3da959a5ea32
terangg <twesa96@gmail.com>
2019-04-15 13:11:46 -0700

    added budgeting stuff

---
+++ Assets/OFC Stuff/Scripts/BudgetSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BudgetSystem : MonoBehaviour
+{
+
+    public Text budgetTxt;
+
+    int budget = 3000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        budgetTxt.text = "Budget: " + budget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateBudget(string tag)
+    {
+        if(tag == "1000") {
+            budget = budget - 1000;
+            budgetTxt.text = "Budget: " + budget;
+        }
+        else if (tag == "500")
+        {
+            budget = budget - 500;
+            budgetTxt.text = "Budget: " + budget;
+        }
+        else if (tag == "250")
+        {
+            budget = budget - 250;
+            budgetTxt.text = "Budget: " + budget;
+        }
+    }
+
+
+}
---
+++ Assets/OFC Stuff/Scripts/DragAndBudget.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragAndBudget : MonoBehaviour
+{
+    Vector3 initialPosition;
+
+    float dragDistance = 9.1f; //distance to drag mouse
+    float zPosSolar;
+    private Vector3 objPos;
+    public GameObject PuzzleCreatorScript;
+    public GameObject BudgetSystem;
+    //public energyScoring engScore;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        initialPosition = gameObject.transform.position;
+
+
+        //dragDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        //Debug.Log(dragDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dragDistance);
+        objPos = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPos;
+    }
+
+
+
+    void OnMouseUp()
+    {
+        //Find the closest Vector3 of the grid, if it returns (0,0,0) -> nothing is close
+
+        Vector3 closest = PuzzleCreatorScript.GetComponent<CreatePuzzle>().GetNearestPointOnGrid(transform.position);
+
+        if (closest == new Vector3(0, 0, 0))
+        {
+            transform.position = initialPosition;
+
+        }
+        else
+        {
+            transform.position = closest;
+            BudgetSystem.GetComponent<BudgetSystem>().UpdateBudget(gameObject.tag);
+            //engScore.count++;
+        }
+    }
+}

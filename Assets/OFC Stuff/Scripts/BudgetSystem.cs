42f888a385211cf537d7f35b92e24984f6316a9d
terangg <twesa96@gmail.com>
2019-04-15 14:13:11 -0700

    added more stuff to budget

--- Assets/OFC Stuff/Scripts/BudgetSystem.cs
+++ Assets/OFC Stuff/Scripts/BudgetSystem.cs
@@ -25,19 +25,45 @@ public class BudgetSystem : MonoBehaviour
     public void UpdateBudget(string tag)
     {
         if(tag == "1000") {
+            Debug.Log(budget + " - 1000");
             budget = budget - 1000;
             budgetTxt.text = "Budget: " + budget;
         }
         else if (tag == "500")
         {
+            Debug.Log(budget + " - 500");
             budget = budget - 500;
             budgetTxt.text = "Budget: " + budget;
         }
         else if (tag == "250")
         {
+            Debug.Log(budget + " - 250");
             budget = budget - 250;
             budgetTxt.text = "Budget: " + budget;
         }
+    }
+
+    public bool ifBudgetNotZero(string tag)
+    {
+        if (tag == "1000")
+        {
+            if ((budget - 1000) < 0)
+            { return false; }
+            else { return true; }
+        }
+        else if (tag == "500")
+        {
+            if ((budget - 500) < 0)
+            { return false; }
+            else { return true; }
+        }
+        else if (tag == "250")
+        {
+            if ((budget - 250) < 0)
+            { return false; }
+            else { return true; }
+        }
+        else { return false; }
     }
 
 
--- Assets/OFC Stuff/Scripts/DragAndBudget.cs
+++ Assets/OFC Stuff/Scripts/DragAndBudget.cs
@@ -6,20 +6,32 @@ using UnityEngine.UI;
 public class DragAndBudget : MonoBehaviour
 {
     Vector3 initialPosition;
+    Quaternion initialRotation;
 
     float dragDistance = 9.1f; //distance to drag mouse
     float zPosSolar;
     private Vector3 objPos;
     public GameObject PuzzleCreatorScript;
     public GameObject BudgetSystem;
+    public GameObject Prefab;
+    bool budgetMoreThanZero;
+
     //public energyScoring engScore;
 
-
+    void Awake()
+    {
+        
+        BudgetSystem = GameObject.Find("BudgetSystem");
+        PuzzleCreatorScript = GameObject.Find("PuzzleCreatorScript");
+        
+    }
     // Use this for initialization
     void Start()
     {
         initialPosition = gameObject.transform.position;
+        initialRotation = gameObject.transform.rotation;
 
+       
 
         //dragDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
         //Debug.Log(dragDistance);
@@ -29,6 +41,11 @@ public class DragAndBudget : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void OnMouseDown()
+    {
+        Instantiate(Prefab, initialPosition, initialRotation);
     }
 
     void OnMouseDrag()
@@ -42,20 +59,29 @@ public class DragAndBudget : MonoBehaviour
 
     void OnMouseUp()
     {
-        //Find the closest Vector3 of the grid, if it returns (0,0,0) -> nothing is close
+        budgetMoreThanZero = BudgetSystem.GetComponent<BudgetSystem>().ifBudgetNotZero(gameObject.tag);
 
-        Vector3 closest = PuzzleCreatorScript.GetComponent<CreatePuzzle>().GetNearestPointOnGrid(transform.position);
-
-        if (closest == new Vector3(0, 0, 0))
+        if (budgetMoreThanZero)
         {
-            transform.position = initialPosition;
+            //Find the closest Vector3 of the grid, if it returns (0,0,0) -> nothing is close
 
+            Vector3 closest = PuzzleCreatorScript.GetComponent<CreatePuzzle>().GetNearestPointOnGrid(transform.position);
+
+            if (closest == new Vector3(0, 0, 0))
+            {
+                //transform.position = initialPosition;
+                Destroy(gameObject);
+            }
+            else
+            {
+                transform.position = closest;
+                BudgetSystem.GetComponent<BudgetSystem>().UpdateBudget(gameObject.tag);
+                //engScore.count++;
+            }
         }
         else
         {
-            transform.position = closest;
-            BudgetSystem.GetComponent<BudgetSystem>().UpdateBudget(gameObject.tag);
-            //engScore.count++;
+            Destroy(gameObject);
         }
     }
 }
